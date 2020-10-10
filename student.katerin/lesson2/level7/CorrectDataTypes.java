c2f4fe7372d0556e44d2ff44705d6d1b0897c5b2
KateRin1103 <katerin110302@gmail.com>
2020-10-10 20:42:16 +0300

    levels 1-7

---
+++ student.katerin/lesson2/level7/CorrectDataTypes.java
@@ -0,0 +1,20 @@
+package student.katerin.lesson2.level7;
+
+//Исправьте ошибки компиляции в данной программе.
+
+public class CorrectDataTypes {
+
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        double numberTwo = 20.0;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+       double sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+    }
+
+}
---
+++ student.katerin/lesson2/level7/SumTwoNumbers.java
@@ -0,0 +1,23 @@
+package student.katerin.lesson2.level7;
+
+//Исправьте ошибки компиляции в данной программе.
+
+public class SumTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int numberOne=0;
+        int numberTwo=0;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+        numberOne = 10;
+        numberTwo = 20;
+
+        int sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+    }
+
+}
