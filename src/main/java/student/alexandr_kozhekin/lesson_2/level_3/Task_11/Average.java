ad8113ae42e9f56a08b9d8c4134543ee4ef907cd
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2020-10-10 17:37:03 +0300

    добавлен метод "average", блок try catch для отлова ошибки о вводе буквы а не цифры, добавдены коментарии, исправлена некоторая выводимая информация

--- src/main/java/student/alexandr_kozhekin/lesson_2/level_3/Task_11/Average.java
+++ src/main/java/student/alexandr_kozhekin/lesson_2/level_3/Task_11/Average.java
@@ -1,19 +1,33 @@
 package student.alexandr_kozhekin.lesson_2.level_3.Task_11;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Average {
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter three values to display the arithmetic mean" + "\n");
-        System.out.println("Enter the first value: ");
-        int firstValue = scanner.nextInt();
-        System.out.println("Enter the second value: ");
-        int secondValue = scanner.nextInt();
-        System.out.println("Enter the third value: ");
-        int thirdValue = scanner.nextInt();
+        //блок для отлова ошибки если вдруг введут не число а букву
+        try {
+            System.out.println("Enter the first value: ");
+            int firstValue = scanner.nextInt();
 
-        double average = (double) (firstValue + secondValue + thirdValue) / 3;
-        System.out.println("Result: " + average);
+            System.out.println("Enter the second value: ");
+            int secondValue = scanner.nextInt();
+
+            System.out.println("Enter the third value: ");
+            int thirdValue = scanner.nextInt();
+
+            System.out.println("Entered values: " +
+                    "|" + firstValue + "|" + secondValue + "|" + thirdValue + "|" + "\n" +
+                    "Result: " + average(firstValue, secondValue, thirdValue));
+        }
+        catch (InputMismatchException e){
+            System.out.println("Invalid value entered!" + "\n" + "Please, enter integer!");
+        }
+    }
+
+    public static double average(int firstValue, int secondValue, int thirdValue){
+        return (double) (firstValue + secondValue + thirdValue) / 3;
     }
 }
