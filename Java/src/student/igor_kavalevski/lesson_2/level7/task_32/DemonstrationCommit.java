bfb4c8c9b25d56bc7a16353c18e1a87b406f55d8
Igor kavaleuski <kavaleuskiihar@gmail.com>
2020-10-11 20:43:43 +0200

    Home work level 1-7

---
+++ Java/src/student/igor_kavalevski/lesson_2/level7/task_32/DemonstrationCommit.java
@@ -0,0 +1,20 @@
+package student.igor_kavalevski.lesson_2.level7.task_32;
+
+public class DemonstrationCommit {
+    public static void main(String[] args) {
+
+        //System.out.println("Hello World");
+        /*int x = 5;
+        int y = 4;
+        int a = x+y
+        System.out.print("+y+ " + "+x+" ="+a);
+         */
+        int y = 5;
+        int x = 5;
+        int a = x+y;
+
+
+        System.out.println(" "+y+" + "+x+" = "+a);
+
+    }
+}
