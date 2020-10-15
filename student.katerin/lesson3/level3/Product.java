9772d5f783a4c1e5ae632cf2834f7ea9807aae3a
KateRin1103 <katerin110302@gmail.com>
2020-10-15 20:04:20 +0300

    lesson3

--- student.katerin/lesson3/level3/Product.java
+++ student.katerin/lesson3/level3/Product.java
@@ -1,5 +1,7 @@
 package level3;
 
+import java.util.Scanner;
+
 public class Product {
     public String name;
     public double regularPrice;
@@ -31,8 +33,25 @@ class ProductDemo {
         product1.discount = 70;
         product1.regularPrice = 150;
 
-        product1.printInformation();
-        System.out.println("NewPrice:" + product1.actualPrice());
+        int i=0;
+
+        while (i != 3) {
+            System.out.println("1)Show info\n2)Show new price\n3)Exit");
+            Scanner in = new Scanner(System.in);
+            System.out.print("Input a number: ");
+            i = in.nextInt();
+
+            switch (i) {
+                case 1:
+                    product1.printInformation();
+                    break;
+                case 2:
+                    System.out.println("NewPrice:" + product1.actualPrice());
+                    break;
+                case 3:
+                    break;
+            }
+        }
     }
 }
 
