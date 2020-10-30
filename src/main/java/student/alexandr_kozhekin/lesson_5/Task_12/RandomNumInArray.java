d0b9a3f017d5e24dc745fa3863b616b15f17a3d6
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2020-10-30 19:29:16 +0300

    выполнено задание 12 уровня 2 лекции 5

---
+++ src/main/java/student/alexandr_kozhekin/lesson_5/Task_12/RandomNumInArray.java
@@ -0,0 +1,17 @@
+package student.alexandr_kozhekin.lesson_5.Task_12;
+
+public class RandomNumInArray {
+    public static void main(String[] args) {
+
+        int [] randomNumArray = new int [3];
+
+        System.out.println("Случайные три числа в массиве: ");
+
+        for (int i = 0; i < randomNumArray.length; i++){
+
+            randomNumArray[i] = (int) (Math.random() * (200+1)) - 100;
+
+            System.out.println(randomNumArray[i]);
+        }
+    }
+}
