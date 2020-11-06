795d4c3900dba6698c5ad8d4d8d483d8bca19e59
Igor kavaleuski <kavaleuskiihar@gmail.com>
2020-11-06 19:07:32 +0100

    Home_work_lecture_6

---
+++ Java/src/student/lesson_6/WordService.java
@@ -0,0 +1,39 @@
+package student.lesson_6;
+
+import java.util.Arrays;
+
+public class WordService {
+
+    public static void main(String[] args) {
+
+
+        String text = "Hello haw haw are are are you";
+
+        String[] ar = text.split(" ");
+
+        Arrays.sort(ar);
+
+        System.out.println(Arrays.toString(ar));
+
+        int max = 0;
+        int count = 1;
+        String word = ar[0];
+        String curr = ar[0];
+
+        for (int i = 1; i < ar.length; i++) {
+
+            if (ar[i].equals(curr)) {
+                count++;
+            } else {
+                count = 1;
+                curr = ar[i];
+            }
+            if (max < count) {
+                max = count;
+                word = ar[i];
+            }
+        }
+        System.out.println(max + " x " + word);
+
+    }
+}
