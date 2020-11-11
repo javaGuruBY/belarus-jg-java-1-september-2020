7542e055257fe8fc44940874e7c41bdb3ae3d3e0
KateRin1103 <katerin110302@gmail.com>
2020-11-11 21:01:40 +0300

    homework7 task1

--- Katerina/homework7/task1/WordService.java
+++ Katerina/homework7/task1/WordService.java
@@ -12,22 +12,21 @@ public class WordService {
         return str.split(" ");
     }
 
-    public void findWord(String[] strArr) {
-        int word1 = 0, word2 = 0, word3 = 0;
-        for (String str :
-                strArr) {
-            if (str.equals("one")) word1++;
-            if (str.equals("two")) word2++;
-            if (str.equals("three")) word3++;
+    public String find(String[] strArr) {
+        int max = 0, count = 1;
+        String maxString = "none";
+        for (int i = 1; i < strArr.length; i++) {
+            String wordToEqual = strArr[i];
+            count = 1;
+            for (int j = i + 1; j < strArr.length; j++) {
+                if (wordToEqual.equals(strArr[j])) count++;
+            }
+            if (count > max) {
+                maxString = wordToEqual;
+                max = count;
+            }
         }
-        System.out.println("The most frequent " + findMax(word1, word2, word3));
-    }
-
-    public String findMax(int word1, int word2, int word3) {
-        if (word1 > word2 && word1 > word3) return "is word 'one'";
-        if (word2 > word1 && word2 > word3) return "is word 'two'";
-        if (word3 > word2 && word3 > word1) return "is word 'three'";
-       // if (word1 > word2 && word1 == word3) return "are words 'one and two'";
-        else return "none";
+        System.out.println("Number:" + max);
+        return maxString;
     }
 }
--- Katerina/homework7/task1/WordServiceDemo.java
+++ Katerina/homework7/task1/WordServiceDemo.java
@@ -5,9 +5,8 @@ public class WordServiceDemo {
     public static void main(String[] args) {
 
         WordService str = new WordService();
-        str.setStr("One two two three three three");
+        str.setStr("One two two three three two two three");
         String[] strArr = str.stringSplit();
-        str.findWord(strArr);
+        System.out.println("Max word is '" + str.find(strArr) + "'");
     }
-
 }
