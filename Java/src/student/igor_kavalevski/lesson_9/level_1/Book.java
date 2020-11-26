6a323d45c772206d75ec5902b659f2b733d83aca
Igor kavaleuski <kavaleuskiihar@gmail.com>
2020-11-26 18:37:17 +0100

    Home_work_lecture_9_L/1

---
+++ Java/src/student/igor_kavalevski/lesson_9/level_1/Book.java
@@ -0,0 +1,28 @@
+package student.igor_kavalevski.lesson_9.level_1;
+
+public class Book {
+
+    String title;
+    String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
---
+++ Java/src/student/igor_kavalevski/lesson_9/level_1/BookListContainsDemo.java
@@ -0,0 +1,34 @@
+package student.igor_kavalevski.lesson_9.level_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookListContainsDemo {
+
+    public static void main(String[] args) {
+
+        List<Book> books = new ArrayList<>();
+
+        Book book1 = new Book("A1", "B1");
+        Book book2 = new Book("A2", "B2");
+
+        books.add(book1);
+        books.add(book2);
+
+        Book bookInTheList = new Book("A1", "B1");
+
+        boolean containsBook = books.contains(book1);
+
+        books.add(bookInTheList);
+
+        System.out.println(containsBook);
+
+        Book bookNotInTheList = new Book("A3", "B3");
+
+        boolean notContainsBook = books.contains(bookNotInTheList);
+
+        System.out.println(notContainsBook);
+
+        System.out.println(books);
+    }
+}
