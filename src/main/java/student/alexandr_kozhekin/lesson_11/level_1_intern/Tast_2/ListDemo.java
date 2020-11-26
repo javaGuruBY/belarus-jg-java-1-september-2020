e19626b25dbb8cfc90a602364174a5ec8fe4283e
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2020-11-26 20:44:08 +0300

    выполнено задание 2 уровня 1 лекции 11

---
+++ src/main/java/student/alexandr_kozhekin/lesson_11/level_1_intern/Tast_2/ListDemo.java
@@ -0,0 +1,15 @@
+package student.alexandr_kozhekin.lesson_11.level_1_intern.Tast_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+    public static void main(String[] args) {
+        //можно хранить ссылки на объекты
+        List<Integer> arrayList1 = new ArrayList<>();
+        List<Object> arrayList2 = new ArrayList<>();
+        //примитивные типы нельзя
+        List<int> arrayList3 = new ArrayList<>();
+
+    }
+}
