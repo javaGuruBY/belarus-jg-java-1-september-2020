f0a1cf7934dc0652e8dd852207bb147e85909e80
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2020-11-26 20:35:18 +0300

    выполнено задание 1 уровня 1 лекции 11

---
+++ src/main/java/student/alexandr_kozhekin/lesson_11/level_1_intern/Task_1/ListDemo.java
@@ -0,0 +1,18 @@
+package student.alexandr_kozhekin.lesson_11.level_1_intern.Task_1;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListDemo {
+    public static void main(String[] args) {
+
+        List<Integer> arrayList = new ArrayList<>();
+        List <Integer> linkedList = new LinkedList<>();
+        /*
+        Отличаются они в реализации ArrayList реализован на массивах а LinkedList реализован на связанном списке
+        а так же в скорости взятия элемента (arrayList быстрее), присваивонии элемента (arrayList быстрее),
+        вставке элемента (LinkedList быстрее) и в удалении элемента (LinkedList быстрее).
+        */
+    }
+}
