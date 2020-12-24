80a47f86e53379af706ae0a31e1f141fb4d1cb98
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2020-12-24 20:12:19 +0300

    выполнено задание 4 лекции 14 уровня 1

--- src/main/java/student/alexandr_kozhekin/lesson_11/level_1_intern/Task_1/FruitStorageTest.java
+++ src/main/java/student/alexandr_kozhekin/lesson_11/level_1_intern/Task_1/FruitStorageTest.java
@@ -1,7 +1,6 @@
 package student.alexandr_kozhekin.lesson_11.level_1_intern.Task_1;
 
 import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,5 +50,44 @@ public class FruitStorageTest {
         }
 
         assertEquals(extend, actual);
+    }
+
+    @Test
+    public void fruitStorageSearchForColorAppleTest() {
+
+        FruitStorage fruitStorage = new FruitStorage();
+
+        List<String> firstExtend = new ArrayList<>();
+        List<String> secondExtend = new ArrayList<>();
+
+        firstExtend.add(fruitStorage.getAllApples().get(0).getColor());
+        firstExtend.add(fruitStorage.getAllApples().get(1).getColor());
+        firstExtend.add(fruitStorage.getAllApples().get(2).getColor());
+        secondExtend.add(fruitStorage.getAllApples().get(3).getColor());
+        secondExtend.add(fruitStorage.getAllApples().get(4).getColor());
+        secondExtend.add(fruitStorage.getAllApples().get(5).getColor());
+
+        List<Apple> appleRedList = fruitStorage.fruitStorageSearchForColorApple(fruitStorage.getAllApples(), "red");
+        List<Apple> appleGreenList = fruitStorage.fruitStorageSearchForColorApple(fruitStorage.getAllApples(), "green");
+
+        List<String> firstActual = new ArrayList<>();
+        List<String> secondActual = new ArrayList<>();
+
+
+        for (int i = 0; i < appleRedList.size(); i++) {
+
+            firstActual.add(appleRedList.get(i).getColor());
+
+        }
+
+        for (int i = 0; i < appleGreenList.size(); i++) {
+
+            secondActual.add(appleGreenList.get(i).getColor());
+
+        }
+
+        assertEquals(firstExtend, firstActual);
+        assertEquals(secondExtend, secondActual);
+
     }
 }
