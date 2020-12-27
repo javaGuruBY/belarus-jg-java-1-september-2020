2ae493f6963c82135a028453f099c49c5a201202
Denis76 <shldenis@yandex.ru>
2020-12-28 01:08:59 +0300

    added src directory

---
+++ src/main/java/com/javaguru/student_Denis_Shlichkov/lesson_3/level_3/RobotDemo.java
@@ -0,0 +1,17 @@
+package com.javaguru.student_Denis_Shlichkov.lesson_3.level_3;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+        Robot robotOne = new Robot("Rider");
+        robotOne.sayHello();
+        robotOne.sayYourName();
+
+        Robot robotTwo = new Robot("John");
+        robotTwo.sayHello();
+        robotTwo.sayYourName();
+
+        Robot robotThree = new Robot("Marduk");
+        robotThree.sayHello();
+        robotThree.sayYourName();
+    }
+}
