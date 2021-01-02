34fc9c6041100dbf401f847e786aa1af48a174c6
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2021-01-02 17:16:12 +0300

    выполнено задание 8 лекции 12 уровня 2

---
+++ src/main/java/student/alexandr_kozhekin/lesson_12/level_2_intern/Task_8/UserCredentials.java
@@ -0,0 +1,15 @@
+package student.alexandr_kozhekin.lesson_12.level_2_intern.Task_8;
+
+import java.util.List;
+
+public class UserCredentials {
+
+    private List<Role> roles;
+
+    public UserCredentials(List<Role> roles) {
+
+        this.roles = roles;
+
+    }
+
+}
