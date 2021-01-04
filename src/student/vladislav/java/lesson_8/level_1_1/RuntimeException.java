d12ac4c7ea5dbfec96ce51838961fcca6368634b
vladislav-domaniewski-L10Lv1T1 <vlad.viktor44@mail.ru>
2021-01-04 23:18:56 +0300

    Lesson 10, level 1, task 3

---
+++ src/student/vladislav/java/lesson_8/level_1_1/RuntimeException.java
@@ -0,0 +1,4 @@
+package student.vladislav.java.lesson_8.level_1_1;
+
+public class RuntimeException {
+}
---
+++ src/student/vladislav/java/lesson_8/level_1_1/Task3.java
@@ -0,0 +1,9 @@
+package student.vladislav.java.lesson_8.level_1_1;
+
+public class Task3 {
+
+        class MyException extends Exception, RuntimeException {
+
+    }
+
+}
