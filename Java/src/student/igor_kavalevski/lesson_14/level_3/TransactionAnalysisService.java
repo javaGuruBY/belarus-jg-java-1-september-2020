c3e8cac6679f7f85d50b09dd6670d04ff28ad66a
Igor kavaleuski <kavaleuskiihar@gmail.com>
2021-01-06 18:55:24 +0100

    Home_work_lecture_14_level_4

--- Java/src/student/igor_kavalevski/lesson_14/level_3/TransactionAnalysisService.java
+++ Java/src/student/igor_kavalevski/lesson_14/level_3/TransactionAnalysisService.java
@@ -2,6 +2,7 @@ package student.igor_kavalevski.lesson_14.level_3;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class TransactionAnalysisService {
@@ -40,5 +41,54 @@ public class TransactionAnalysisService {
                 .filter(s -> s.getYear() == 2011)
                 .sorted(Comparator.comparing(Transaction::getValue))
                 .collect(Collectors.toList());
+    }
+
+    public List<Integer> findAllTransactionOfYear(List<Transaction> transactions) {
+
+        return transactions.stream()
+                .map(Transaction::getYear)
+                .collect(Collectors.toList());
+    }
+
+    public Set<Integer> findAllUniqueYearTransaction(List<Transaction> transactions) {
+
+        return transactions.stream()
+                .map(Transaction::getYear)
+                .collect(Collectors.toSet());
+    }
+
+    public Set<String> findNameUniqueTraders(List<Transaction> transactions) {
+
+        return transactions.stream()
+                .map(Transaction::getTrader)
+                .map(Trader::getName)
+                .collect(Collectors.toSet());
+    }
+
+    public Set<String> findCityUniqueTraders(List<Transaction> transactions) {
+
+        return transactions.stream()
+                .map(Transaction::getTrader)
+                .map(Trader::getCity)
+                .collect(Collectors.toSet());
+    }
+
+    public Set<String> findNamesTraderForCambridge(List<Transaction> transactions) {
+
+        return transactions.stream()
+                .map(Transaction::getTrader)
+                .filter(s -> s.getCity().equals("Cambridge"))
+                .map(Trader::getName)
+                .collect(Collectors.toSet());
+
+    }
+
+    public Set<String> findNamesTraderFromCity(List<Transaction> transactions, String cityFind) {
+
+        return transactions.stream()
+                .map(Transaction::getTrader)
+                .filter(s -> s.getCity().equals(cityFind))
+                .map(Trader::getName)
+                .collect(Collectors.toSet());
     }
 }
--- Java/src/student/igor_kavalevski/lesson_14/level_3/TransactionAnalysisServiceTest.java
+++ Java/src/student/igor_kavalevski/lesson_14/level_3/TransactionAnalysisServiceTest.java
@@ -2,9 +2,7 @@ package student.igor_kavalevski.lesson_14.level_3;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -92,5 +90,102 @@ public class TransactionAnalysisServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void findAllTransactionOfYearTest() {
+
+        List<Integer> expected = new ArrayList<>();
+
+        expected.add(transData.getTransactions().get(0).getYear());
+        expected.add(transData.getTransactions().get(1).getYear());
+        expected.add(transData.getTransactions().get(2).getYear());
+        expected.add(transData.getTransactions().get(3).getYear());
+        expected.add(transData.getTransactions().get(4).getYear());
+        expected.add(transData.getTransactions().get(5).getYear());
+
+        List<Transaction> transaction = transData.getTransactions();
+
+        List<Integer> actual = trans.findAllTransactionOfYear(transaction);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findAllUniqueYearTransactionTest() {
+
+        Set<Integer> expected = new LinkedHashSet<>();
+
+        expected.add(transData.getTransactions().get(0).getYear());
+        expected.add(transData.getTransactions().get(1).getYear());
+
+        List<Transaction> transaction = transData.getTransactions();
+
+        Set<Integer> actual = trans.findAllUniqueYearTransaction(transaction);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findNameUniqueTradersTest() {
+
+        Set<String> expected = new HashSet<>();
+
+        expected.add(transData.getTransactions().get(1).getTrader().getName());
+        expected.add(transData.getTransactions().get(0).getTrader().getName());
+        expected.add(transData.getTransactions().get(5).getTrader().getName());
+        expected.add(transData.getTransactions().get(3).getTrader().getName());
+
+        List<Transaction> transaction = transData.getTransactions();
+
+        Set<String> actual = trans.findNameUniqueTraders(transaction);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findCityUniqueTradersTest() {
+
+        Set<String> expected = new HashSet<>();
+
+        expected.add(transData.getTransactions().get(0).getTrader().getCity());
+        expected.add(transData.getTransactions().get(3).getTrader().getCity());
+
+        List<Transaction> transaction = transData.getTransactions();
+
+        Set<String> actual = trans.findCityUniqueTraders(transaction);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findNameTradersCambridgeTest() {
+
+        Set<String> expected = new HashSet<>();
+
+        expected.add(transData.getTransactions().get(0).getTrader().getName());
+        expected.add(transData.getTransactions().get(1).getTrader().getName());
+        expected.add(transData.getTransactions().get(5).getTrader().getName());
+
+        List<Transaction> transaction = transData.getTransactions();
+
+        Set<String> actual = trans.findNamesTraderForCambridge(transaction);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findNamesTraderFromCityTest() {
+
+        Set<String> expected = new HashSet<>();
+
+        expected.add(transData.getTransactions().get(3).getTrader().getName());
+
+        List<Transaction> transaction = transData.getTransactions();
+
+        Set<String> actual = trans.findNamesTraderFromCity(transaction, "Milan");
+
+        assertEquals(expected, actual);
+    }
 }
+
 
